92e0307da754a09d65fb76286a0c27b1e9eb4d49
davidrmh <davidricardo888@hotmail.com>
2023-02-22 18:39:05 +0100

    Add function cross_val_states_em:

--- gauss_hmm.py
+++ gauss_hmm.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def fit_model_em(model,
                  train_data,
                  num_states,
@@ -19,4 +22,15 @@ def fit_model_em(model,
     params, loglike = hmm.fit_em(params,
                                  prop,
                                  train_data)
-    return params, loglike
+    return hmm, params, loglike
+
+def cross_val_states_em(model, states, folds, init_method, key):
+    for i, s in enumerate(states):
+        print(f'=== Starting fitting with {s} states === \n')
+        avgloglike = np.zeros(len(states))
+        for f in folds:
+            train, test = f
+            hmm ,par, loglike = fit_model_em(model, train, s, init_method, key)
+            avgloglike[i] += hmm.marginal_log_prob(par, test)
+    avgloglike = avgloglike / len(folds)
+    return avgloglike
