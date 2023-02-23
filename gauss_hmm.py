66ac042205e5637871f9024c787a773784fc57f6
davidrmh <davidricardo888@hotmail.com>
2023-02-23 18:50:21 +0100

    Add psi function

--- gauss_hmm.py
+++ gauss_hmm.py
@@ -1,4 +1,5 @@
 import numpy as np
+import jax.numpy as jnp
 
 
 def fit_model_em(model,
@@ -25,13 +26,33 @@ def fit_model_em(model,
                                  train_data)
     return hmm, params, loglike
 
-def cross_val_states_em(model, states, folds, init_method, key):
+def cross_val_states_em(model, states, folds, init_method, key, **kwargs):
     avgloglike = np.zeros(len(states))
     for i, s in enumerate(states):
-        print(f'=== Starting fitting with {s} states === \n')
+        print(f'\n === Starting fitting with {s} states === \n')
         for f in folds:
             train, test = f
-            hmm ,par, loglike = fit_model_em(model, train, s, init_method, key)
+            hmm ,par, loglike = fit_model_em(model, train, s, init_method, key, **kwargs)
             avgloglike[i] += hmm.marginal_log_prob(par, test)
     avgloglike = avgloglike / len(folds)
     return avgloglike
+
+def psi(mu1, mu2, cov1, cov2, beta = 0.5):
+    cov1_inv = jnp.linalg.inv(cov1)
+    cov2_inv = jnp.linalg.inv(cov2)
+    cov_dag = jnp.linalg.inv(cov1_inv + cov2_inv)
+    mu_dag = cov1_inv @ mu1 + cov2_inv @ mu2
+    det_cov_dag = jnp.linalg.det(cov_dag)
+    det_cov1 = jnp.linalg.det(cov1)
+    det_cov2 = jnp.linalg.det(cov2)
+    
+    # To avoid nan
+    if det_cov1 == 0 or det_cov2 == 0:
+        return 0
+    
+    quad_cov1 = mu1.T @ cov1_inv @ mu1
+    quad_cov2 = mu2.T @ cov2_inv @ mu2
+    quad_cov_dag = mu_dag.T @ cov_dag @ mu_dag
+    factor1 = (det_cov_dag ** 0.5) * (det_cov1 ** (-0.5 * beta)) * (det_cov2 ** (-0.5 * beta))
+    factor2 = jnp.exp(-0.5 * beta * (quad_cov1 + quad_cov2 - quad_cov_dag))
+    return factor1 * factor2
